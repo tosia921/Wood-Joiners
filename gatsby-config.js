c08215ffca1b6750df7660008c2d3c59bb101e29
Tomasz Posiadala <tomaszposiadala@gmail.com>
2020-12-06 13:38:48 +0000

    First commit, cleared gatsby-default-starter from unnecessary files, added gloogle Fonts, styled-components plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,15 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `teko\:400,600,700`
+        ]
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,20 +24,5 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
---
+++ src/components/styles/GlobalStyles.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+import bodyBackground from '../../images/bodyBackground.jpg'
+ 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-image: 
+        linear-gradient(
+            rgba(0, 0, 0, 0.2), 
+            rgba(0, 0, 0, 0.2)
+            ),
+        url(${bodyBackground});
+    background-size: cover;
+    background-repeat: repeat;
+
+    padding: 1.5rem calc((100vw - 1400px) / 2);
+    @media screen and (max-width: 1400px) {
+        padding: 1.5rem 3rem;
+    }
+    
+    }
+
+    *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-family: Teko, Helvetica, Sans-Serif;
+    font-size: 62.5%;
+    @media screen and (max-width: 768px) {
+        font-size: 40%;
+    }
+    @media screen and (max-width: 1000px) {
+        font-size: 50%;
+    }
+    }
+
+    
+  
+`;
+ 
+export default GlobalStyle;
+
+
+
+
