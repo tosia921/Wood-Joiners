e4c70e917f65b4ab5645a5bb187854ca1c92d38d
Tomasz Posiadala <tomaszposiadala@gmail.com>
2020-12-28 20:31:01 +0000

    added content to testimonial slider, styled it, added RatedPeople.com logo as link to company profile

--- src/components/TestimonialsSection/TestimonialSection.component.js
+++ src/components/TestimonialsSection/TestimonialSection.component.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { SectionContainer, SectionBanner, BannerText, StyledPtag, StyledCarousel, StyledCarouselItem } from './TestimonialSection.styles';
+import { SectionContainer, SectionBanner, BannerText, StyledPtag, StyledCarousel, StyledCarouselItem, CustomerComment, CustomerName, RatedPeopleLogo } from './TestimonialSection.styles';
+
+
 
 
 const TestimonialSection = () => {
@@ -15,18 +17,34 @@ const TestimonialSection = () => {
 
             <StyledCarousel>
                 <StyledCarouselItem>
-                    
+                       
+                        <CustomerComment>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula aliquet odio, eu maximus dolor gravida eu. Cras luctus pharetra risus eu interdum.
+                        In ultrices eros nec risus congue pulvinar a et quam. Ut ultricies enim dui, sit amet posuere est finibus at. 
+                        Cras quam ligula, ultricies sit amet convallis.</CustomerComment>
+                        <CustomerName>Mark Johnson</CustomerName>
                     
                 </StyledCarouselItem>
                 <StyledCarouselItem>
                     
+                        <CustomerComment>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula aliquet odio, eu maximus dolor gravida eu. Cras luctus pharetra risus eu interdum.
+                        </CustomerComment>
+                        <CustomerName>Tomasz Posiadala</CustomerName>
                     
                 </StyledCarouselItem>
                 <StyledCarouselItem>
                     
-                    
+                        <CustomerComment>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula aliquet odio, eu maximus dolor gravida eu. Cras luctus pharetra risus eu interdum.
+                        In ultrices eros nec risus congue pulvinar a et quam. </CustomerComment>
+                        <CustomerName>Amy Wine</CustomerName>
+                        
                 </StyledCarouselItem>
             </StyledCarousel>
+
+            <StyledPtag>CHECK REST OF OUR AMAZING RATING AND COMMENTS AT:</StyledPtag>
+            <a href="#">
+                <RatedPeopleLogo/>
+            </a>
+
         
         </SectionContainer>
     )
--- src/components/TestimonialsSection/TestimonialSection.styles.js
+++ src/components/TestimonialsSection/TestimonialSection.styles.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import { Carousel } from 'react-bootstrap';
 
-import WoodenBanner from '../../images/SectionBanners/sectionbanner4.png'
+import WoodenBanner from '../../images/SectionBanners/sectionbanner4.png';
+import RatedPeopleimg from '../../images/RatedPeopleLogo.png';
 
 export const SectionContainer = styled.div`
     height: fit-content;
@@ -34,8 +35,8 @@ export const BannerText = styled.h2`
     
 `
 export const StyledPtag = styled.p`
-    font-size: 3rem;
-    font-weight: 400;
+    font-size: 3.5rem;
+    font-weight: 500;
     color: ${({ theme }) => theme.PrimaryTextColor};
     text-shadow: ${({ theme }) => theme.TextShadowBig};
     margin: 4rem 0;
@@ -48,8 +49,45 @@ export const StyledCarousel = styled(Carousel)`
     background-color: rgba(255,255,255,0.2);
     border-radius: 20px;
     margin-bottom: 4rem;
+    position: relative;
+    
 `
 
 export const StyledCarouselItem = styled(Carousel.Item)`
-   
+   position: relative;
+   min-height: 100%;
+   margin-top: 7rem;
+`
+
+export const CustomerComment = styled.p`
+
+    width: 60%;
+    font-size: 2.3rem;
+    color: ${({ theme }) => theme.PrimaryTextColor};
+    text-shadow: ${({ theme }) => theme.TextShadowBig};
+    margin-left: auto;
+    margin-right: auto;
+    
+`
+
+export const CustomerName = styled.p`
+    text-align: right;
+    width: 75%;
+    margin-top: -1rem;
+    font-size: 3rem;
+    color: ${({ theme }) => theme.PrimaryTextColor};
+    text-shadow: ${({ theme }) => theme.TextShadowBig};
+`
+
+export const RatedPeopleLogo = styled.div`
+    height: 30rem;
+    width: 30rem;
+    background-image: url(${RatedPeopleimg});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    transition: all 0.3s ease-in;
+    &:hover {
+        transform: scale(1.1);
+    }
 `
