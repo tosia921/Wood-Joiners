e865efcd8069217c871e6314c6cd9b73d4a13b27
Tomasz Posiadala <tomaszposiadala@gmail.com>
2021-01-12 12:44:10 +0000

    fixed problem with creating pages dynamicly in gatsby-node.js file, also changed the tamplate file(that still needs to be coded to accept data from headles cms )

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,39 @@
-exports.createPages = async function ({ actions, graphql }) {
-    const { data } = await graphql(`
-      query {
+const path = require("path")
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  // Query for markdown nodes to use in creating pages.
+  const result = await graphql(
+    `
+      {
         allContentfulProject {
           edges {
             node {
-                slug
+              slug
             }
           }
         }
       }
-    `)
-    data.allContentfulProject.edges.forEach(edge => {
-      const slug = edge.node.slug;
-      actions.createPage({
-        path: `projects/${slug}`,
-        component: require.resolve(`./src/templates/project.js`),
-        context: { slug: slug },
-      })
-    })
+    `
+  )
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
   }
+  // Create pages for each markdown file.
+  const ProjectTemplate = path.resolve(`src/templates/project.js`)
+  result.data.allContentfulProject.edges.forEach(({ node }) => {
+    const path = `projects/${node.slug}`
+    createPage({
+      path,
+      component: ProjectTemplate,
+      // In your blog post template's graphql query, you can use pagePath
+      // as a GraphQL variable to query for data from the markdown file.
+      context: {
+        pagePath: path,
+      },
+    })
+  })
+}
