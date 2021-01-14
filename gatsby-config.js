d47946f99d01bb84f15a484fdc94a3fd49dc3c8c
Tomasz Posiadala <tomaszposiadala@gmail.com>
2021-01-14 09:09:19 +0000

    working on project.js file to display rich content field from contenftul

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,22 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    `@contentful/gatsby-transformer-contentful-richtext`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
--- src/templates/project.js
+++ src/templates/project.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from 'react';
+import { graphql } from 'gatsby';
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { BLOCKS } from "@contentful/rich-text-types"
+import Layout from '../components/layout';
 
-const Project = () => {
+
+const Project = ({data}) => {
+    const RichContent = JSON.parse(data.contentfulProject.projectContent.raw);
+    const options = {
+        renderNode: {
+            [BLOCKS.HEADING_1]: (node, children) => <h1>{children}</h1>,
+            // [BLOCKS.EMBEDDED_ASSET]:  <img src={`https:${data.contentfulProject.projectContent.references.fluid.src}`}/>,
+        },
+    renderMark: {},
+    }
+    console.log(data)
     return (
         <Layout>
-            <h1>blabbkla</h1>
+            
+            {documentToReactComponents(RichContent, options)}
         </Layout>
     )
 }
 
 export default Project;
+
+export const query = graphql`
+    query($id: String!) {
+        contentfulProject(id: {eq: $id}) {
+                title
+                projectContent  {
+                    raw
+                    references {
+                            fluid {
+                                    src
+                                    
+                                }
+                        }
+                    }
+                }
+            }
+        
+
+`
